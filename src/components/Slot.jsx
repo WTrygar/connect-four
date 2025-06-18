d2707bc09720c0bd9fc9ab40d4001a12dbbdd745
WTrygar <chandelier.wt@gmail.com>
2025-06-18 10:19:13 +0200

    feat: algorythm that checks if there's 4 in a row almost ready

--- src/components/Slot.jsx
+++ src/components/Slot.jsx
@@ -1,7 +1,18 @@
-const Slot = () => {
-  return <div>
-    <h4>Slot</h4>
-  </div>;
+import redToken from "../assets/images/counter-red-small.svg";
+import yellowToken from "../assets/images/counter-yellow-small.svg";
+
+const Slot = ({ ch, y, x }) => {
+  return (
+    <div className="slot" x={x} y={y}>
+      {ch && (
+        <img
+          src={ch === "X" ? redToken : yellowToken}
+          width="100%"
+          height="100%"
+        />
+      )}
+    </div>
+  );
 };
 
-export default Slot
+export default Slot;
